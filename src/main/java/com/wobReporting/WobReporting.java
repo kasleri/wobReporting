dbf7a78c4183ba5a1add23af4633d7f5b9d1e339
KasiHUN <47517556+KasiHUN@users.noreply.github.com>
2022-04-23 09:58:03 +0200

    Initial commit

---
+++ src/main/java/com/wobReporting/WobReporting.java
@@ -0,0 +1,12 @@
+package com.wobReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WobReporting {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WobReporting.class, args);
+    }
+}
